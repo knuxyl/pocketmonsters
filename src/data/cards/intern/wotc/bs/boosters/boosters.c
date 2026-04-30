ac6a329cffc5212fafc84f1f1c2c055ccdd801f4
knuxyl <jamesdarrellwatson@gmail.com>
2026-04-30 16:57:15 -0500

    significant booster pack update

--- src/data/cards/intern/wotc/bs/boosters/boosters.c
+++ src/data/cards/intern/wotc/bs/boosters/boosters.c
@@ -3,3 +3,71 @@
 #include "bs_booster2_png.c"
 #include "bs_booster3_png.c"
 #include "out.c"
+
+
+struct booster {
+	struct embedded normal;
+	struct embedded normal_fe;
+	struct embedded rare;
+	struct embedded rare_fe;
+}
+/*
+
+first 8 packs are first edition
+1/8192 chance of getting rare pack, 5 only rares
+rare pack can be fe if got before 16 packs
+
+
+bs/1 charizard
+bs/2 blastoise
+bs/3 venusaur
+ju/1 wigglytuff
+ju/2 scyther
+ju/3 flareon
+fo/1 aerodactyl
+fo/2 zapdos
+fo/3 lapras
+tr/1 gyarados
+tr/2 persian
+tr/3 meowth
+tr/4 golbat
+bs2/1 pidgeot
+bs2/2 magneton
+bs2/3 gyarados
+bs2/4 mewtwo
+gh/1 onix
+gh/2 gloom
+gh/3 raichu
+gh/4 starmie
+gc/1 arcanine
+gc/2 nidoking
+gc/3 weezing
+gc/4 alakazam
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
